140bfd27dec0212ccab3ed8363084aef022f55f7
GoutamK1902 <goutamkhowal321@gmail.com>
2025-08-04 14:40:13 +0530

    deploy2.0.3

--- app/profile/[userId]/page.tsx
+++ app/profile/[userId]/page.tsx
@@ -1,28 +1,30 @@
-// app/profile/[userId]/page.tsx
 import Posts from "@/components/Posts";
 import UserProfileHeader from "@/components/UserProfileHeader";
 import { getAllPostAction } from "@/lib/serveractions";
-import { IPost } from "@/models/post.model";
-
-type PageProps = {
-  params: { userId: string };
-};
+import type { IPost } from "@/models/post.model";
 
 async function filteredPost(userId: string) {
   const posts = await getAllPostAction();
   return posts.filter((post: IPost) => post.user.userId === userId);
 }
 
-const UserProfile = async ({ params }: PageProps) => {
+export default async function UserProfile({
+  params,
+}: {
+  params: { userId: string }
+}) {
   const userPosts = await filteredPost(params.userId);
   console.log(params.userId);
   console.log(typeof userPosts);
+
+  if (!userPosts.length) {
+    return <p>No posts found for this user.</p>;
+  }
+
   return (
     <div className="flex-1 mt-14 pt-5">
       <UserProfileHeader user={userPosts[0].user} />
       <Posts posts={userPosts} />
     </div>
   );
-};
-
-export default UserProfile;
+}
